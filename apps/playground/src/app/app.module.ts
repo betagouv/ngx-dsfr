8601b18331940271ea2bb6d11b696c35ad2b0d17
Florian Gesta <flo.gesta@gmail.com>
2022-05-18 11:25:38 +0200

    Working on DsfrLinkComponent

--- apps/playground/src/app/app.module.ts
+++ apps/playground/src/app/app.module.ts
@@ -1,13 +1,24 @@
+/**
+ * Angular imports
+ */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/**
+ * 3rd-party imports
+ */
+import { DsfrLinkModule } from '@betagouv/ngx-dsfr/link';
+
+/**
+ * Internal imports
+ */
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, DsfrLinkModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- libs/ngx-dsfr/link/src/lib/link.component.ts
+++ libs/ngx-dsfr/link/src/lib/link.component.ts
@@ -1,7 +1,15 @@
 /**
  * Angular imports
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+/**
+ * TypeScript entities and constants
+ */
+export const EMPTY_LABEL_ERROR: string =
+  'You MUST provide a label for this link 😡 !!!';
+export const EMPTY_HREF_ERROR: string =
+  'You MUST provide a value for the href attribute 😡 !!!';
 
 @Component({
   selector: 'dsfr-link',
@@ -9,7 +17,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./link.component.scss']
 })
 export class DsfrLinkComponent implements OnInit {
+  @Input() label: string = '';
+  @Input() link: string = '';
+
+  isExternal: boolean = false;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (!this.label) {
+      throw EMPTY_LABEL_ERROR;
+    }
+    if (!this.link) {
+      throw EMPTY_HREF_ERROR;
+    }
+
+    this.isExternal = this.link.indexOf('http') > -1;
+  }
 }
