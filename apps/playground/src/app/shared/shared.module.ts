d565207949009fe4e1c46ea580bfe54ef5ac00fc
Florian Gesta <flo.gesta@gmail.com>
2022-12-09 16:59:11 +0100

    chore: [nx migration] migration-v15

--- apps/playground/src/app/shared/shared.module.ts
+++ apps/playground/src/app/shared/shared.module.ts
@@ -4,10 +4,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
+import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
 
 /**
  * Internal imports
