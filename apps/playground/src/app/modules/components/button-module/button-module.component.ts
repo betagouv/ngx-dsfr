b65f4a94d95290893d583812b9d14d60ef3960e1
Abdelaziz SAKHI <abdelaziz.sakhi@gmail.com>
2023-02-06 18:20:11 +0100

    feat(button): add documentation

--- apps/playground/src/app/modules/components/button-module/button-module.component.ts
+++ apps/playground/src/app/modules/components/button-module/button-module.component.ts
@@ -2,7 +2,13 @@
  * Angular imports
  */
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+
+/**
+ * 3rd-party imports
+ */
+import { ElementAlignment, ElementSize } from '@betagouv/ngx-dsfr';
+import { ButtonHtmlType, ButtonType } from '@betagouv/ngx-dsfr/button';
 
 @Component({
   templateUrl: './button-module.component.html',
@@ -10,17 +16,54 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ButtonModuleComponent implements OnInit {
 
-  form: FormGroup | undefined;
+  buttonForm: FormGroup | undefined;
+  nbButtonClicked: number = 0;
 
-  constructor(private formBuilder: FormBuilder) { }
+  possibleTypes: Record<string, string> = {
+    'primary': 'primary',
+    'secondary': 'secondary',
+    'tertiary': 'tertiary',
+    'tertiary-no-outline': 'tertiary-no-outline',
+    'close': 'close'
+  };
+
+  possibleIconAlignments: Record<string, string> = {
+    'left': 'left',
+    'right': 'right'
+  };
+
+  possibleHtmlTypes: Record<string, string> = {
+    'button': 'button',
+    'submit': 'submit',
+    'reset': 'reset'
+  };
+
+  possibleSizes: Record<string, string> = {
+    'sm': 'sm',
+    'md': 'md',
+    'lg': 'lg'
+  };
+
+  constructor(private formBuilder: NonNullableFormBuilder) { }
 
   ngOnInit(): void {
     this.initForms();
   }
 
   private initForms(): void {
-    this.form = this.formBuilder.group({
-      label: 'DSFR Button works 😁'
+    this.buttonForm = this.formBuilder.group({
+      label: new FormControl('This is a beautiful button', [Validators.required]),
+      title: new FormControl('This is a beautiful title', [Validators.required]),
+      disabled: new FormControl(false, [Validators.required]),
+      size: new FormControl(ElementSize.MEDIUM, [Validators.required]),
+      htmlType: new FormControl(ButtonHtmlType.BUTTON, [Validators.required]),
+      type: new FormControl(ButtonType.PRIMARY, [Validators.required]),
+      icon: new FormControl('fr-icon-checkbox-circle-line', [Validators.required]),
+      iconAlignment: new FormControl(ElementAlignment.LEFT, [Validators.required]),
     });
+  }
+
+  handleClick() {
+    this.nbButtonClicked++;
   }
 }
