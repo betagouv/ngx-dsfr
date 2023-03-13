1035e3ebee5ea96c4bb68e6036ef40ba62482651
Anas Ouahidi <anasouahidi@gmail.com>
2023-03-13 03:26:03 +0100

    fix review

--- apps/playground/src/app/modules/components/badge-module/badge-module.component.ts
+++ apps/playground/src/app/modules/components/badge-module/badge-module.component.ts
@@ -7,8 +7,17 @@ import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@ang
 /**
  * 3rd-party imports
  */
-import { ElementSize } from '@betagouv/ngx-dsfr';
-import { ThemeColor } from '@betagouv/ngx-dsfr';
+import { ElementSize, ThemeColor } from '@betagouv/ngx-dsfr';
+
+/**
+ * TypeScript entities and constants
+ */
+interface BadgeForm {
+  label: FormControl<string>;
+  theme: FormControl<ThemeColor>;
+  noIcon: FormControl<boolean>;
+  size: FormControl<ElementSize>;
+}
 
 @Component( {
   templateUrl: './badge-module.component.html',
@@ -16,41 +25,12 @@ import { ThemeColor } from '@betagouv/ngx-dsfr';
 } )
 export class BadgeModuleComponent implements OnInit {
 
-  badgeForm: FormGroup | undefined;
+  badgeForm: FormGroup<BadgeForm> | undefined;
   theme: typeof ThemeColor = ThemeColor;
 
-  possibleThemes: Record<string, string> = {
-    'info': 'info',
-    'new': 'new',
-    'warning': 'warning',
-    'success': 'success',
-    'error': 'error',
-    'blue-france': 'blue-france',
-    'red-marianne': 'red-marianne',
-    'grey': 'grey',
-    'green-tilleul-verveine': 'green-tilleul-verveine',
-    'green-bourgeon': 'green-bourgeon',
-    'green-emeraude': 'green-emeraude',
-    'green-menthe': 'green-menthe',
-    'green-archipel': 'green-archipel',
-    'blue-ecume': 'blue-ecume',
-    'blue-cumulus': 'blue-cumulus',
-    'purple-glycine': 'purple-glycine',
-    'pink-macaron': 'pink-macaron',
-    'pink-tuile': 'pink-tuile',
-    'yellow-tournesol': 'yellow-tournesol',
-    'yellow-moutarde': 'yellow-moutarde',
-    'orange-terre-battue': 'orange-terre-battue',
-    'brown-cafe-creme': 'brown-cafe-creme',
-    'brown-caramel': 'brown-caramel',
-    'brown-opera': 'brown-opera',
-    'beige-gris-galet': 'beige-gris-galet'
-  };
+  possibleThemes: typeof ThemeColor = ThemeColor;
 
-  possibleSizes: Record<string, string> = {
-    'sm': 'sm',
-    'md': 'md'
-  };
+  possibleSizes: typeof ElementSize = ElementSize;
 
   constructor( private formBuilder: NonNullableFormBuilder ) {
   }
@@ -61,10 +41,10 @@ export class BadgeModuleComponent implements OnInit {
 
   private initForms(): void {
     this.badgeForm = this.formBuilder.group( {
-      label: new FormControl( 'This is a label', [ Validators.required ] ),
-      theme: new FormControl( ThemeColor.SUCCESS, [ Validators.required ] ),
-      noIcon: new FormControl( false, [ Validators.required ] ),
-      size: new FormControl( ElementSize.MEDIUM, [ Validators.required ] )
+      label: [ 'This is a label', [ Validators.required ] ],
+      theme: [ ThemeColor.SUCCESS, [ Validators.required ] ],
+      noIcon: [ false, [ Validators.required ] ],
+      size: [ ElementSize.MEDIUM, [ Validators.required ] ]
     } );
   }
 }
