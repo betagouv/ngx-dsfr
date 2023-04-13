9d47876144038d7fadf0933eb2a5e8424b145311
Azizoou <abdelaziz.sakhi@gmail.com>
2023-04-13 02:36:30 +0200

    code review fixes

--- apps/playground/src/app/modules/components/input-module/input-module.component.ts
+++ apps/playground/src/app/modules/components/input-module/input-module.component.ts
@@ -2,7 +2,26 @@
  * Angular imports
  */
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormControlStatus, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+
+/**
+ * Angular imports
+ */
+import { InputType } from '@betagouv/ngx-dsfr/input';
+import { Subject, takeUntil } from 'rxjs';
+
+interface FormInput {
+  label: FormControl<string>,
+  type: FormControl<InputType>,
+  placeholder: FormControl<string>,
+  hint: FormControl<string>,
+  icon: FormControl<string>,
+  disabled: FormControl<boolean>,
+  hasFailed: FormControl<boolean>,
+  hasSucceeded: FormControl<boolean>,
+  failureMessage: FormControl<string>,
+  successMessage: FormControl<string>,
+}
 
 @Component({
   templateUrl: './input-module.component.html',
@@ -10,7 +29,10 @@ import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 })
 export class InputModuleComponent implements OnInit {
 
+  private unsubscribe$ = new Subject<void>();
+
   inputForm: FormGroup | undefined;
+  inputFormErrors: Record<string, string> = {};
   form: FormGroup | undefined;
 
   possibleInputTypes: Record<string, string> = {
@@ -42,6 +64,39 @@ export class InputModuleComponent implements OnInit {
 
     this.form = this.formBuilder.group({
       input: ''
-    })
+    });
+
+
+    this.handleErrors('Please, enter a label for this Component', 'label');
+    this.handleErrors('Please, enter a failure meessage for this Component', 'failureMessage');
+  }
+
+  private handleErrors(errorMsg: string, controlName?: string): void {
+    if (controlName) {
+      this.inputForm
+        ?.get(controlName)
+        ?.statusChanges.pipe(takeUntil(this.unsubscribe$))
+        .subscribe({
+          next: (value: FormControlStatus) => {
+            if (value === 'INVALID') {
+              this.inputFormErrors[controlName] = errorMsg;
+            } else {
+              this.inputFormErrors = {};
+            }
+          }
+        });
+    } else {
+      this.inputForm?.statusChanges
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe({
+          next: (value: FormControlStatus) => {
+            if (value === 'INVALID') {
+              this.inputFormErrors['_form_'] = errorMsg;
+            } else {
+              this.inputFormErrors = {};
+            }
+          }
+        });
+    }
   }
 }
