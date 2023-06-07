e81a2832b7e438de520ab3a6c5dbb2c97f65d99c
Anas Ouahidi <anasouahidi@gmail.com>
2023-06-07 09:44:24 +0200

    fix stepper component

--- apps/playground/src/app/modules/components/stepper-module/stepper-module.component.ts
+++ apps/playground/src/app/modules/components/stepper-module/stepper-module.component.ts
@@ -2,10 +2,19 @@
  * Angular imports
  */
 import { Component } from '@angular/core';
+import { NonNullableFormBuilder } from '@angular/forms';
 
 @Component({
   templateUrl: './stepper-module.component.html',
   styleUrls: ['./stepper-module.component.scss']
 })
 export class StepperModuleComponent {
+  formStepper = this.formBuilder.group({
+    stepTitle: 'The step\'s title',
+    stepNumber: 1,
+    numberOfSteps: 3,
+    nextStepTitle: 'The next step\'s title'
+  });
+
+  constructor(private formBuilder: NonNullableFormBuilder) {}
 }
