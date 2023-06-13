0b536e10a1badad97ff70661e2e7a44010e02b58
Anas Ouahidi <anasouahidi@gmail.com>
2023-06-13 11:50:34 +0200

    fix review 2

--- apps/playground/src/app/modules/components/stepper-module/stepper-module.component.ts
+++ apps/playground/src/app/modules/components/stepper-module/stepper-module.component.ts
@@ -1,14 +1,17 @@
 /**
  * Angular imports
  */
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { createErrorHandling } from '../../validation/handle-errors';
 
 @Component({
   templateUrl: './stepper-module.component.html',
   styleUrls: ['./stepper-module.component.scss']
 })
 export class StepperModuleComponent {
+  stepperFormErrors: Record<string, string> = {};
+
   formStepper = this.formBuilder.group({
     stepTitle: ['The step\'s title', Validators.required],
     stepNumber: 1,
@@ -16,5 +19,26 @@ export class StepperModuleComponent {
     nextStepTitle: 'The next step\'s title'
   });
 
+  private readonly handleErrors = createErrorHandling(inject(DestroyRef));
+
   constructor(private formBuilder: NonNullableFormBuilder) {}
+
+  ngOnInit(): void {
+    this.handleRequiredErrors();
+  }
+
+  private handleRequiredErrors(): void {
+    this.handleErrors({
+      form: this.formStepper,
+      controlName: 'stepTitle',
+      formErrors: this.stepperFormErrors,
+      error: 'Please, enter a title for the current step'
+    });
+    this.handleErrors({
+      form: this.formStepper,
+      controlName: 'numberOfSteps',
+      formErrors: this.stepperFormErrors,
+      error: 'Please, enter the number of steps for this stepper'
+    });
+  }
 }
