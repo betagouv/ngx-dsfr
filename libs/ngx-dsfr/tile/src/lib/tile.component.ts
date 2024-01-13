7aa0c700a36837f4bb0548b27dc25c184a08c528
Azizoou <abdelaziz.sakhi@gmail.com>
2024-01-13 17:12:22 +0100

    Adding download property tu Tile and Link component

--- libs/ngx-dsfr/tile/src/lib/tile.component.ts
+++ libs/ngx-dsfr/tile/src/lib/tile.component.ts
@@ -2,6 +2,10 @@
  * Angular imports
  */
 import { Component, Input, OnChanges } from '@angular/core';
+
+/**
+ * 3rd-party imports
+ */
 import { Breakpoint } from '@betagouv/ngx-dsfr';
 
 /**
@@ -11,12 +15,11 @@ export const EMPTY_LINK_ERROR: string =
   'You MUST provide a value for the link attribute 😡 !!!';
 export const EMPTY_TITLE_ERROR: string =
   'You MUST provide a value for the title attribute 😡 !!!';
-
 export type TemplateAlign = 'horizontal' | 'vertical';
-
 enum TemplateType {
   INTERNAL,
-  EXTERNAL
+  EXTERNAL,
+  DOWNLOAD
 }
 
 @Component({
@@ -32,6 +35,8 @@ export class DsfrTileComponent implements OnChanges {
   @Input() title: string | undefined;
   @Input() image: string | undefined;
   @Input() description: string | undefined;
+  @Input() detail: string | undefined;
+  @Input() download: boolean = false;
 
   classes: string = '';
   template: TemplateType = TemplateType.INTERNAL;
@@ -48,15 +53,33 @@ export class DsfrTileComponent implements OnChanges {
       throw EMPTY_LINK_ERROR;
     }
 
-    this.template = this.link.indexOf('http') > -1
-      ? TemplateType.EXTERNAL
-      : TemplateType.INTERNAL;
+    this.setTemplateType();
+  }
+
+  private setTemplateType(): void {
+    this.template = TemplateType.INTERNAL;
+
+    if (
+      this.link &&
+      this.link.indexOf('http') > -1 &&
+      !this.download
+    ) {
+      this.template = TemplateType.EXTERNAL
+    }
+
+    if (this.download) {
+      this.template = TemplateType.DOWNLOAD
+    }
   }
 
   private initClasses(): void {
     this.classes = `fr-tile fr-enlarge-link fr-tile--${this.align}`;
     if (this.breakpoint) {
       this.classes += `-${this.breakpoint}`;
+    }
+
+    if (this.download) {
+      this.classes += ' fr-tile--download'
     }
   }
 
